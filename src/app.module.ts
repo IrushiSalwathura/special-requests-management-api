d5daf5e74d1879d9ac21004146e9cd72d9432557
IrushiSalwathura <irushitharindi@gmail.com>
2025-01-30 18:43:20 +0530

    Implement request APIs :zap:

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { RequestModule } from './request/request.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/requestmgtdb'),
+    UserModule,
+    RequestModule,
   ],
   controllers: [AppController],
   providers: [AppService],
